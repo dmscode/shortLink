db085e77a2d348a9ae66209ebfdcf138042f4de4
dmscode <i@zji.me>
2021-10-01 14:56:48 +0800

    Init

---
+++ source/build.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+const filedir = process.cwd()+'/'
+
+const urlsSource = JSON.parse(fs.readFileSync(filedir+'source/urls.json'))
+const urls = {}
+
+let markCounter = 0
+for(const u of urlsSource){
+  for(const m of u.mark){
+    if(urls[m]){
+      console.log('遇到重复标记: '+m+', url: '+u.url)
+      continue
+    }
+    urls[m] = u.url
+    markCounter++
+  }
+}
+console.log("共计生成 "+urlsSource.length+" 个地址，"+markCounter+' 个标记。')
+const webpage = fs.readFileSync(filedir+'source/index.html').toString().replace(/```urls```/, JSON.stringify(urls))
+fs.writeFile(filedir+'index.html',
+  webpage,
+  (err) => {
+    if (err) {
+      return console.error(err);
+    }
+  })
+  console.log('Success: 共计生成 '+urlsSource.length+' 个地址，'+markCounter+' 个标记。')
