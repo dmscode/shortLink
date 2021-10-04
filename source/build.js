809a3c3859c95b465784f0f56be099672220031e
dmscode <i@zji.me>
2021-10-04 15:56:22 +0800

    readme

--- source/build.js
+++ source/build.js
@@ -1,11 +1,13 @@
 const fs = require('fs')
 const filedir = process.cwd()+'/'
 
-const urlsSource = JSON.parse(fs.readFileSync(filedir+'source/urls.json'))
+const config = JSON.parse(fs.readFileSync(filedir+'source/config.json'))
 const urls = {}
 
 let markCounter = 0
-for(const u of urlsSource){
+const urlsLength = config.urls.length
+let urlsList = `| Name | Target link | Mark link |\n|------|------------|------------|\n`
+for(const u of config.urls){
   for(const m of u.mark){
     if(urls[m]){
       console.warn('\t- 遇到重复标记: '+m+', url: '+u.url)
@@ -14,8 +16,14 @@ for(const u of urlsSource){
     urls[m] = u.url
     markCounter++
   }
+  urlsList += '| '+u.name+' | '+u.url+' | '+u.mark.map(m=>config.baseUrl+'#'+m).join(' |\n|  |  | ')+' |\n'
 }
-const webpage = fs.readFileSync(filedir+'source/index.html').toString().replace(/```urls```/, JSON.stringify(urls))
+config.urls = JSON.stringify(urls)
+let webpage = fs.readFileSync(filedir+'source/index.html').toString()
+for(const key in config){
+  const reg = new RegExp('```'+key+'```', 'g')
+  webpage = webpage.replace(reg, config[key])
+}
 fs.writeFile(filedir+'index.html',
   webpage,
   (err) => {
@@ -23,4 +31,11 @@ fs.writeFile(filedir+'index.html',
       return console.error(err);
     }
   })
-  console.log('Success: 共计生成 '+urlsSource.length+' 个地址，'+markCounter+' 个标记。')
+fs.writeFile(filedir+'urlsList.md',
+  urlsList,
+  (err) => {
+    if (err) {
+      return console.error(err);
+    }
+  })
+console.log('Success: 共计生成 '+urlsLength+' 个地址，'+markCounter+' 个标记。')
---
+++ urlsList.md
@@ -0,0 +1,10 @@
+| Name | Target link | Mark link |
+|------|------------|------------|
+| 小众软件 | https://www.appinn.com/ | https://r.izyx.xyz/#app |
+|  |  | https://r.izyx.xyz/#appinn |
+| 小众软件 - 论坛 | https://meta.appinn.net/ | https://r.izyx.xyz/#meta |
+| Github | https://github.com/ | https://r.izyx.xyz/#gh |
+|  |  | https://r.izyx.xyz/#github |
+| Gist | https://gist.github.com/ | https://r.izyx.xyz/#gist |
+| 稻米鼠的小脚本们 | https://script.izyx.xyz/ | https://r.izyx.xyz/#script |
+| 笔记，卡片，小书签 | https://meta.appinn.net/t/26446 | https://r.izyx.xyz/#note |
